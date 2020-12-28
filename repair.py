f897f037c1c45614c41cee7d2ee42147b3394fe1
ngoclp2000 <nindaphi@gmail.com>
2020-12-28 17:32:08 +0700

    .

--- repair.py
+++ repair.py
@@ -1,28 +1,10 @@
-file = open("./data/phone_and_table/Maytinhbang.csv","r",encoding='utf-8')
+file = open("./data/phone_and_table/smart_phone.csv","r",encoding='utf-8')
 file2 = open("data.csv","w",encoding='utf-8')
 
 for line in file:
     arr_words = line.split(',')
-    count = 9999999
-    string = ""
-    for i in range(len(arr_words)):
-        if i == 0:
-            string += str( arr_words[i])
-            string += ","
-        elif i > count:
-            string += arr_words[i]
-            if i != len(arr_words) -1:
-                string += ','
-        elif "₫" not in arr_words[i]:
-            string += arr_words[i]
-            string += ' '
-        elif "₫" in arr_words[i]:
-            string += ','
-            string += arr_words[i]
-            string += ','
-            count = i
-        
-    file2.write(string)
+    print(arr_words)
+    break
         
 
 
